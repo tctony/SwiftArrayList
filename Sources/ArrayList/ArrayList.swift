739b5891590cc50e7ce61ae30437253a256ddabc
Tony Tang <tangchang21@gmail.com>
2021-04-15 15:32:18 +0800

    ArrayList

--- Sources/ArrayList/ArrayList.swift
+++ Sources/ArrayList/ArrayList.swift
@@ -1,3 +1,130 @@
-struct ArrayList {
-    var text = "Hello, World!"
+/// An class wrapper of swift Array
+
+public class ArrayList<T> : Sequence {
+
+    public final var elements: Array<T>
+
+    public init(_ elements: Array<T>) {
+        self.elements = elements
+    }
+
+    public convenience init() {
+        self.init([])
+    }
+
+    public convenience init(_ elements: T...) {
+        self.init(elements)
+    }
+
+    public convenience init(_ list: ArrayList<T>) {
+        self.init(list.elements)
+    }
+
+    public subscript(index: Int) -> T {
+        get {
+            return elements[index]
+        }
+        set {
+            elements[index] = newValue
+        }
+    }
+
+    // does return value needs wrap?
+    public subscript(subRange: Range<Int>) -> ArraySlice<T> {
+        get {
+            return elements[subRange]
+        }
+        set {
+            elements[subRange] = newValue
+        }
+    }
+
+    @inlinable public func append(newElement: T) {
+        elements.append(newElement)
+    }
+
+    @inlinable public func insert(newElement: T, atIndex index: Int) {
+        elements.insert(newElement, at: index)
+    }
+
+    @inlinable
+    @discardableResult
+    public func removeAtIndex(index: Int) -> T {
+        return elements.remove(at: index)
+    }
+
+    @inlinable
+    @discardableResult
+    public func removeLast() -> T {
+        return elements.removeLast()
+    }
+
+    @inlinable public func removeAll(keepingCapacity: Bool = false) {
+        elements.removeAll(keepingCapacity: keepingCapacity)
+    }
+
+    @inlinable public func reserveCapacity(minimumCapacity: Int) {
+        elements.reserveCapacity(minimumCapacity)
+    }
+
+    @inlinable public var count: Int {
+        get {
+            return elements.count
+        }
+    }
+
+    @inlinable public var isEmpty: Bool {
+        get {
+            return elements.isEmpty
+        }
+    }
+
+    @inlinable public var capacity: Int {
+        get {
+            return elements.capacity
+        }
+    }
+
+    @inlinable public func sort(by areInIncreasingOrder: (T, T) -> Bool) {
+        elements.sort(by: areInIncreasingOrder)
+    }
+
+    @inlinable public func reverse() {
+        elements.reverse()
+    }
+
+    @inlinable public func filter(includeElement: (T) -> Bool) -> ArrayList<T> {
+        return ArrayList(elements.filter(includeElement))
+    }
+
+    @inlinable public func map<U>(transform: (T) -> U) -> ArrayList<U> {
+        return ArrayList<U>(elements.map(transform))
+    }
+
+    @inlinable public func reduce<U>(initial: U, combine: (U, T) -> U) -> U {
+        return elements.reduce(initial, combine)
+    }
+
+    // MARK: -
+
+    public __consuming func makeIterator() -> Array<T>.Iterator {
+        return elements.makeIterator()
+    }
+
+    // MARK: -
+}
+
+@inlinable
+func += <T>(lhs: inout ArrayList<T>, rhs: T) {
+    lhs.elements += [rhs]
+}
+
+@inlinable
+func += <T>(lhs: inout ArrayList<T>, rhs: Array<T>) {
+    lhs.elements += rhs
+}
+
+@inlinable 
+func += <T>(lhs: inout ArrayList<T>, rhs: ArrayList<T>) {
+    lhs.elements += rhs.elements
 }
--- Tests/ArrayListTests/ArrayListTests.swift
+++ Tests/ArrayListTests/ArrayListTests.swift
@@ -1,12 +1,40 @@
 import XCTest
 @testable import ArrayList
 
+func check(builder: ()-> ArrayList<Int>, array: Array<Int>) {
+    XCTAssertTrue(builder().elements == array)
+}
+
 final class ArrayListTests: XCTestCase {
+
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ArrayList().text, "Hello, World!")
+        check(builder: { ArrayList() }, array: [])
+        check(builder: { ArrayList([1]) }, array: [1])
+        check(builder: { ArrayList(1, 2) }, array: [1, 2])
+        let list = ArrayList(1, 2, 3)
+        check(builder: { ArrayList(list) }, array: [1, 2, 3])
+        XCTAssertTrue(list[0] == 1)
+        XCTAssertTrue(list[0..<2] == [1, 2])
+        check(builder: { () -> ArrayList<Int> in
+            list.append(newElement: 4)
+            return list
+        }, array: [1,2,3,4])
+        check(builder: { () -> ArrayList<Int> in
+            list.insert(newElement: 3, atIndex: 3)
+            return list
+        }, array: [1,2,3,3,4])
+        check(builder: { () -> ArrayList<Int> in
+            list.removeAtIndex(index: 1)
+            return list
+        }, array: [1,3,3,4])
+        check(builder: { () -> ArrayList<Int> in
+            list.removeLast()
+            return list
+        }, array: [1,3,3])
+        check(builder: { () -> ArrayList<Int> in
+            list.removeAll()
+            return list
+        }, array: [])
     }
 
     static var allTests = [
